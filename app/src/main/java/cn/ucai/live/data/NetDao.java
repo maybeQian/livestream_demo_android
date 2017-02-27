dad40ac422251da077fb875ed8d6eda21e660d36
xiongqian <736182683@qq.com>
2017-02-27 17:18:32 +0800

    添加NetDao

---
+++ app/src/main/java/cn/ucai/live/data/NetDao.java
@@ -0,0 +1,76 @@
+package cn.ucai.live.data;
+
+import android.content.Context;
+
+import com.hyphenate.chat.EMGroup;
+
+import java.io.File;
+
+import cn.ucai.live.I;
+import cn.ucai.live.utils.MD5;
+import cn.ucai.live.utils.OkHttpUtils;
+import cn.ucai.live.utils.OnCompleteListener;
+
+
+/**
+ * Created by Administrator on 2017/2/8 0008.
+ */
+
+public class NetDao {
+    public static void register(Context context, String username, String nick, String password,
+                                OnCompleteListener<String> listener) {
+        OkHttpUtils<String> utils = new OkHttpUtils<>(context);
+        utils.setRequestUrl(I.REQUEST_REGISTER)
+                .addParam(I.User.USER_NAME,username)
+                .addParam(I.User.NICK,nick)
+                .addParam(I.User.PASSWORD, MD5.getMessageDigest(password))
+                .post()
+                .targetClass(String.class)
+                .execute(listener);
+    }
+    public static void unRegister(Context context, String username,
+                                OnCompleteListener<String> listener) {
+        OkHttpUtils<String> utils = new OkHttpUtils<>(context);
+        utils.setRequestUrl(I.REQUEST_UNREGISTER)
+                .addParam(I.User.USER_NAME,username)
+                .targetClass(String.class)
+                .execute(listener);
+    }
+
+    public static void login(Context context, String username, String password, OnCompleteListener<String> listener) {
+        OkHttpUtils<String> utils = new OkHttpUtils<>(context);
+        utils.setRequestUrl(I.REQUEST_LOGIN)
+                .addParam(I.User.USER_NAME,username)
+                .addParam(I.User.PASSWORD,MD5.getMessageDigest(password))
+                .targetClass(String.class)
+                .execute(listener);
+    }
+    public static void getUserInfoByUsername(Context context, String username, OnCompleteListener<String> listener) {
+        OkHttpUtils<String> utils = new OkHttpUtils<>(context);
+        utils.setRequestUrl(I.REQUEST_FIND_USER)
+                .addParam(I.User.USER_NAME,username)
+                .targetClass(String.class)
+                .execute(listener);
+    }
+    public static void updateUserNick(Context context,String username,String newNick,OnCompleteListener<String> listener) {
+        OkHttpUtils<String> utils = new OkHttpUtils<>(context);
+        utils.setRequestUrl(I.REQUEST_UPDATE_USER_NICK)
+                .addParam(I.User.USER_NAME,username)
+                .addParam(I.User.NICK,newNick)
+                .targetClass(String.class)
+                .execute(listener);
+    }
+
+    public static void updateAvatar(Context context, String nameorhxid, File file, OnCompleteListener<String> listener) {
+        OkHttpUtils<String> utils = new OkHttpUtils<>(context);
+        utils.setRequestUrl(I.REQUEST_UPDATE_AVATAR)
+                .addParam(I.NAME_OR_HXID,nameorhxid)
+                .addParam(I.AVATAR_TYPE,I.AVATAR_TYPE_USER_PATH)
+                .addFile2(file)
+                .post()
+                .targetClass(String.class)
+                .execute(listener);
+
+    }
+
+}
