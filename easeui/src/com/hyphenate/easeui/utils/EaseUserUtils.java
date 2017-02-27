7cf9ead4b8e77a93abe5320ff854e5283f3d7ac7
xiongqian <736182683@qq.com>
2017-02-27 14:10:28 +0800

    对比超级微信,添加EaseUI中的实体类及相关代码

--- easeui/src/com/hyphenate/easeui/utils/EaseUserUtils.java
+++ easeui/src/com/hyphenate/easeui/utils/EaseUserUtils.java
@@ -1,6 +1,7 @@
 package com.hyphenate.easeui.utils;
 
 import android.content.Context;
+import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -10,9 +11,10 @@ import com.hyphenate.easeui.R;
 import com.hyphenate.easeui.controller.EaseUI;
 import com.hyphenate.easeui.controller.EaseUI.EaseUserProfileProvider;
 import com.hyphenate.easeui.domain.EaseUser;
+import com.hyphenate.easeui.domain.User;
 
 public class EaseUserUtils {
-    
+    private static final String TAG = "EaseUserUtils";
     static EaseUserProfileProvider userProvider;
     
     static {
@@ -30,7 +32,14 @@ public class EaseUserUtils {
         
         return null;
     }
-    
+
+    public static User getAppUserInfo(String username){
+        if(userProvider != null)
+            return userProvider.getAppUser(username);
+
+        return null;
+    }
+
     /**
      * set user avatar
      * @param username
@@ -49,7 +58,52 @@ public class EaseUserUtils {
             Glide.with(context).load(R.drawable.ease_default_avatar).into(imageView);
         }
     }
-    
+
+    public static void setAppUserAvatar(Context context, String username, ImageView imageView){
+    	User user = getAppUserInfo(username);
+        if(user != null && user.getAvatar() != null){
+            setAppUserAvatarByPath(context,user.getAvatar(),imageView);
+        } else if (username != null) {
+            setAppUserAvatarByPath(context,user.getAvatar(),imageView);
+        } else {
+            Glide.with(context).load(R.drawable.ease_default_avatar).into(imageView);
+        }
+    }
+
+    public static void setAppUserAvatarByPath(Context context, String path, ImageView imageView) {
+        if (path != null) {
+            try {
+                int avatarResId = Integer.parseInt(path);
+                Glide.with(context).load(avatarResId).into(imageView);
+            } catch (Exception e) {
+                //use default avatar
+                Glide.with(context).load(path).diskCacheStrategy(DiskCacheStrategy.ALL).placeholder(R.drawable.ease_default_avatar).into(imageView);
+            }
+        }else{
+            Glide.with(context).load(R.drawable.ease_default_avatar).into(imageView);
+        }
+    }
+
+    public static void setAppGroupAvatar(Context context, String hxid, ImageView imageView) {
+        if (hxid != null) {
+            try {
+                int avatarResId = Integer.parseInt(getGroupAvatarPath(hxid));
+                Glide.with(context).load(avatarResId).into(imageView);
+            } catch (Exception e) {
+                //use default avatar
+                Glide.with(context).load(getGroupAvatarPath(hxid)).diskCacheStrategy(DiskCacheStrategy.ALL).placeholder(R.drawable.ease_group_icon).into(imageView);
+            }
+        }else{
+            Glide.with(context).load(R.drawable.ease_group_icon).into(imageView);
+        }
+    }
+
+    private static String getGroupAvatarPath(String hxid) {
+        String path = "http://101.251.196.90:8000/SuperWeChatServerV2.0/downloadAvatar?name_or_hxid="
+                +hxid+"&avatarType=group_icon&m_avatar_suffix=.jpg";
+        return path;
+    }
+
     /**
      * set user's nickname
      */
@@ -63,5 +117,18 @@ public class EaseUserUtils {
         	}
         }
     }
-    
+
+    public static void setAppUserNick(String username,TextView textView){
+        if(textView != null){
+        	User user = getAppUserInfo(username);
+            if(user != null && user.getMUserNick() != null){
+        		textView.setText(user.getMUserNick());
+        	}else{
+        		textView.setText(username);
+        	}
+        }
+    }
+
+
+
 }
