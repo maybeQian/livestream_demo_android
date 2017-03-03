658fa62fd44bf371e53e66a0d1067cfaa9224cd9
xiongqian <736182683@qq.com>
2017-03-03 11:01:39 +0800

    直播的观众头像显示

--- app/src/main/java/cn/ucai/live/ui/activity/LiveBaseActivity.java
+++ app/src/main/java/cn/ucai/live/ui/activity/LiveBaseActivity.java
@@ -501,11 +501,8 @@ public abstract class LiveBaseActivity extends BaseActivity {
           showUserDetailsDialog(namelist.get(position));
         }
       });
-      //暂时使用测试数据
-      Glide.with(context)
-          .load(avatarRepository.getAvatar())
-          .placeholder(R.drawable.ease_default_avatar)
-          .into(holder.Avatar);
+      EaseUserUtils.setAppUserAvatar(context,namelist.get(position),holder.Avatar);
+
     }
 
     @Override public int getItemCount() {
